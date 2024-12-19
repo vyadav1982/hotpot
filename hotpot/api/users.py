c48c81ad7f5197b8767522847f7b972369f1afa4
Shashikant Yadav <sashikant12rao@gmail.com>
2024-12-19 12:22:32 +0000

    api:created new api

--- hotpot/api/users.py
+++ hotpot/api/users.py
@@ -1,4 +1,6 @@
 import frappe
+from datetime import datetime
+import pytz
 
 
 @frappe.whitelist(methods=["GET"])
@@ -13,9 +15,58 @@ def get_list():
 		fields=["name", "employee_id", "employee_name"],
 		filters=[
 			["is_active", "=", 1],
-			["is_guest", "=", 0],
+			# ["is_guest", "=", 0],
 		],
 	)
+
+
+@frappe.whitelist(allow_guest=True)
+def update_coupon_count(params):
+	print("+++" * 10, params)
+	params = frappe.parse_json(params)
+	employee_id = params.get("userId")
+	coupon_count = params.get("token")
+	coupon = params.get("coupon")
+	print(coupon)
+	doc = frappe.get_doc("Hotpot User", employee_id)
+	doc.coupon_count = int(coupon_count) + 1
+	doc.save()
+	frappe.db.commit()
+	doc = frappe.new_doc("Hotpot Coupons History")
+	doc.employee_id = employee_id
+	doc.type = "Cancellation"
+	doc.message = f"You Cancelled a coupon for {coupon['title']} ({coupon['coupon_date']})"
+	doc.insert()
+	return {"message": "Coupon count updated successfully"}
+
+
+@frappe.whitelist(allow_guest=True)
+def get_coupons_history(employee_id):
+	result = []
+	# params = frappe.parse_json(params)
+	# employee_id = params.get("userId")
+	query = """ SELECT modified_by, data, docname, creation FROM tabVersion WHERE docname = %s AND ref_doctype LIKE '%%Hotpot%%' ORDER BY creation DESC LIMIT 10; """
+	result = frappe.db.sql(query, (employee_id), as_dict=True)
+	result += frappe.db.get_list(
+		"Hotpot Coupons History",
+		fields=["employee_id", "type", "message", "creation"],
+		filters=[["employee_id", "=", employee_id]],
+		order_by="creation desc",
+		limit=25,
+	)
+	return result
+
+
+def reset_coupon_count():
+	users = frappe.db.get_list(
+		"Hotpot User", filters=[["is_active", "=", 1], ["is_guest", "=", 0]], pluck="name"
+	)
+
+	for name in users:
+		doc = frappe.get_doc("Hotpot User", name)
+		doc.coupon_count = 60
+		doc.save()
+	frappe.db.commit()
 
 
 @frappe.whitelist(methods=["POST"])
