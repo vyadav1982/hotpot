874008f1cc6b24a9334448015ca9c77ee7e9340d
Shashikant Yadav <sashikant12rao@gmail.com>
2024-12-30 11:42:01 +0000

    ui: "modified the serving component"

--- frontend/src/routes/server.tsx
+++ frontend/src/routes/server.tsx
@@ -4,22 +4,18 @@ import QrReader from 'react-qr-reader';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { useFrappePostCall } from 'frappe-react-sdk';
+import { useFrappePostCall, useFrappeUpdateDoc } from 'frappe-react-sdk';
 import { ErrorBanner } from '@/components/ErrorBanner';
-import { Camera, CookingPot, TriangleAlert } from 'lucide-react';
-import {
-  coupon_from_info,
-  extractCouponInfo,
-  is_coupon_valid,
-} from '@/utils/coupon';
+import { Camera, CookingPot, Loader2, TriangleAlert } from 'lucide-react';
+import { coupon_from_info, extractCouponInfo } from '@/utils/coupon';
 import { useDialog } from '@/hooks/use-dialog';
 import { HotpotCoupon } from '@/types/Hotpot/HotpotCoupon';
 import { ProtectedRoute } from '@/utils/auth/ProtectedRoute';
 import { UserContext } from '@/utils/auth/UserProvider';
 import { TopBar } from '@/components/TopBar';
 import { Link } from '@tanstack/react-router';
-import useCurrentCouponType from '@/hooks/useCurrentCouponType';
 import { Logo } from '@/components/Logo';
+import { useToast } from '@/hooks/use-toast';
 
 export const Route = createFileRoute('/server')({
   component: () => (
@@ -29,15 +25,29 @@ export const Route = createFileRoute('/server')({
   ),
 });
 
+type MealTiming = {
+  name: string;
+  start_hour: number;
+  end_hour: number;
+  buffer_time: number;
+};
+
 function ServerPage() {
   const { currentUser, logout } = useContext(UserContext);
-  const { currentCouponType } = useCurrentCouponType();
   const [scanning, setScanning] = useState(false);
   const [scannedData, setScannedData] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [couponExists, setCouponExists] = useState<boolean | null>(null);
   const [delayScan] = useState(500);
   const [isReaderReady, setIsReaderReady] = useState(false);
+  const { updateDoc, loading } = useFrappeUpdateDoc();
+  const [couponType, setCouponType] = useState<MealTiming[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
+  const [disableServe, setDisableServe] = useState(false);
+  const { call: getCouponType } = useFrappePostCall(
+    'hotpot.api.dashboard.get_coupon_type_list',
+  );
   const [couponDoc, setCouponDoc] = useState<HotpotCoupon | undefined>(
     undefined,
   );
@@ -47,34 +57,33 @@ function ServerPage() {
     'hotpot.api.coupon.coupon_exists',
   );
 
-  const {
-    call: createCoupon,
-    error: apiCreateError,
-    loading: creatingCoupon,
-  } = useFrappePostCall('hotpot.api.coupon.create_coupon');
-
   const handleScan = useCallback(
     async (data: string | null) => {
       if (data) {
         setScanning(false);
         setScannedData(data);
+        setIsLoading(true);
         const processedData = extractCouponInfo(data);
         if (processedData) {
           try {
             const response = await checkCouponExists({
               name: processedData,
             });
-            setCouponExists(!!response.message);
             if (response.message) {
+              setIsLoading(false);
               setCouponDoc(response.message);
+            } else {
+              setCouponDoc(undefined);
+              setIsLoading(false);
             }
           } catch (error) {
             console.error('Error checking coupon:', error);
             setError('Error checking coupon status. Please try again.');
             setCouponExists(null);
+            setIsLoading(false);
           }
         } else {
-          setError('Invalid QR code format');
+          setIsLoading(false);
         }
       }
     },
@@ -109,36 +118,51 @@ function ServerPage() {
     setTimeout(() => setIsReaderReady(true), 100);
   }, []);
 
-  const handleCreateCoupon = async () => {
-    if (!scannedData) return;
+  function formatHour(hour: any) {
+    const h = Math.floor(hour / 100);
+    const m = hour % 100;
+    return `${h.toString().padStart(2, '0')}:${m.toString().padStart(2, '0')}`;
+  }
 
-    try {
-      await showConfirmDialog({
-        title: 'Issue Meal',
-        description:
-          'Are you sure you want to issue this meal? This action cannot be undone.',
-        confirmLabel: 'Confirm',
-        variant: 'default',
-        isLoading: creatingCoupon,
-        onConfirm: async () => {
-          const response = await createCoupon({
-            data: scannedData,
+  async function handleServeCoupon(couponDoc: any) {
+    setDisableServe(false);
+    return new Promise<void>((resolve, reject) => {
+      updateDoc('Hotpot Coupon', couponDoc.name, {
+        docstatus: 1,
+        served_by: currentUser,
+      })
+        .then(() => {
+          resolve();
+          setDisableServe(true);
+          toast({
+            title: 'Success',
+            description: `Meal served successfully.`,
+            className:
+              'bg-green-100 text-green-600 border border-green-300 rounded-lg shadow-lg p-4 my-2 flex items-center gap-2',
           });
-          if (response.message) {
-            if (!response.message.created) {
-              setCouponExists(!response.message.created);
-            }
-            setCouponDoc(response.message.coupon);
-          }
-        },
-      });
-    } catch (error) {
-      console.error('Error creating coupon:', error);
-      setError('Error creating coupon. Please try again.');
-      setCouponExists(null);
-    }
-  };
+        })
+        .catch((error) => {
+          toast({
+            variant: 'destructive',
+            title: `Error`,
+            description: "Couldn't serve meal. Please try again.",
+            className:
+              'bg-red-100 text-red-600 border border-red-300 rounded-lg shadow-lg p-4 my-2 flex items-center gap-2',
+          });
+          reject(error);
+          setError(error);
+        });
+    });
+  }
 
+  function getMealTimeRange(title: string | undefined) {
+    const meal = couponType.find((meal) => meal.name === title);
+    if (!meal) {
+      return { startHour: 0, endHour: 0 };
+    }
+
+    return { startHour: meal.start_hour, endHour: meal.end_hour };
+  }
   const handleLogout = async () => {
     try {
       await showConfirmDialog({
@@ -156,7 +180,12 @@ function ServerPage() {
   };
 
   useEffect(() => {
+    const fetchCouponTypes = async () => {
+      const response = await getCouponType({});
+      setCouponType(response.message);
+    };
     return () => {
+      fetchCouponTypes();
       setScanning(false);
       setIsReaderReady(false);
     };
@@ -235,111 +264,176 @@ function ServerPage() {
                 </div>
               )}
 
-              {scannedData && (
-                <div className="space-y-4">
-                  {couponExists === false && !!scannedData && !couponDoc ? (
-                    <>
-                      <Alert
-                        className={
-                          is_coupon_valid(
-                            coupon_from_info(scannedData),
-                            currentCouponType,
-                          )
-                            ? ''
-                            : 'bg-red-50'
-                        }
-                      >
-                        {is_coupon_valid(
-                          coupon_from_info(scannedData),
-                          currentCouponType,
-                        ) ? (
-                          <AlertTitle>Coupon yet to be Served</AlertTitle>
-                        ) : (
-                          <AlertTitle className="flex items-center gap-2">
-                            <TriangleAlert className="text-red-600" />
-                            ERROR: Coupon Cannot be Served
-                          </AlertTitle>
-                        )}
-                        <AlertDescription>
-                          <>
-                            <p>
-                              Employee:{' '}
-                              {coupon_from_info(scannedData)?.employee_id}
-                            </p>
-                            <p>Type: {coupon_from_info(scannedData)?.title}</p>
-                            <p>
-                              Time: {coupon_from_info(scannedData)?.coupon_time}
-                            </p>
-                            <p>
-                              Date: {coupon_from_info(scannedData)?.coupon_date}
-                            </p>
-                          </>
-                        </AlertDescription>
-                      </Alert>
-                      <Button
-                        onClick={() => handleCreateCoupon()}
-                        className="w-full"
-                        disabled={
-                          !is_coupon_valid(
-                            coupon_from_info(scannedData),
-                            currentCouponType,
-                          )
-                        }
-                      >
-                        {is_coupon_valid(
-                          coupon_from_info(scannedData),
-                          currentCouponType,
-                        )
-                          ? 'Issue ' + currentCouponType
-                          : 'Invalid / Expired Coupon'}
-                      </Button>
-                    </>
-                  ) : couponExists === true ? (
-                    <Alert className="bg-red-50">
-                      <AlertTitle className="gap-2">
-                        <TriangleAlert className="text-red-600" />
-                        ERROR: Meal has already been Served.
-                      </AlertTitle>
-                      <AlertDescription>
-                        <>
-                          <p>Type: {couponDoc?.title}</p>
-                          <p>Time: {couponDoc?.coupon_time}</p>
-                          <p>Date: {couponDoc?.coupon_date}</p>
-                          <p>Served By: {couponDoc?.served_by}</p>
-                        </>
-                      </AlertDescription>
-                    </Alert>
-                  ) : (
-                    <Alert className="bg-green-50">
-                      <AlertTitle className="gap-2">
-                        <CookingPot className="text-green-600" />
-                        SUCCESS: Meal Served{' '}
-                      </AlertTitle>
-                      <AlertDescription>
-                        <>
-                          <p>Type: {couponDoc?.title}</p>
-                          <p>Time: {couponDoc?.coupon_time}</p>
-                          <p>Date: {couponDoc?.coupon_date}</p>
-                          <p>Served By: {couponDoc?.served_by}</p>
-                        </>
-                      </AlertDescription>
-                    </Alert>
-                  )}
-
-                  <Button
-                    onClick={handleReset}
-                    variant="outline"
-                    className="w-full"
-                  >
-                    Scan Another
-                  </Button>
+              {isLoading ? (
+                <div className="flex items-center justify-center">
+                  <Loader2 className="h-12 w-12 animate-spin text-primary" />
                 </div>
+              ) : (
+                scannedData && (
+                  <div className="space-y-4">
+                    {(() => {
+                      const couponInfo = coupon_from_info(scannedData);
+
+                      if (!couponInfo) {
+                        return (
+                          <Alert className="bg-red-300">
+                            <AlertTitle className="flex items-center gap-2">
+                              <TriangleAlert className="text-red-600" />
+                              ERROR: Invalid Coupon Scanned
+                            </AlertTitle>
+                            <AlertDescription>
+                              <p>
+                                The scanned data does not match any valid coupon
+                                format.
+                              </p>
+                            </AlertDescription>
+                          </Alert>
+                        );
+                      }
+
+                      if (!couponDoc) {
+                        return (
+                          <Alert className="bg-red-300">
+                            <AlertTitle className="flex items-center gap-2">
+                              <TriangleAlert className="text-red-600" />
+                              ERROR: Coupon Not Found
+                            </AlertTitle>
+                            <AlertDescription>
+                              <p>The coupon does not exist in our records.</p>
+                            </AlertDescription>
+                          </Alert>
+                        );
+                      }
+
+                      const { coupon_date, coupon_time, title } = couponInfo;
+                      const { startHour, endHour } = getMealTimeRange(title);
+                      const currentTime = new Date();
+                      if (
+                        new Date().toISOString().split('T')[0] !== coupon_date
+                      ) {
+                        return (
+                          <Alert className="bg-yellow-400">
+                            <AlertTitle className="flex items-center gap-2">
+                              <TriangleAlert className="text-yellow-600" />
+                              NOTICE: Coupon Not Valid for Today
+                            </AlertTitle>
+                            <AlertDescription>
+                              <p>The coupon is valid only for {coupon_date}.</p>
+                            </AlertDescription>
+                          </Alert>
+                        );
+                      }
+
+                      const currentHour = parseInt(
+                        `${currentTime.getHours()}${currentTime.getMinutes().toString().padStart(2, '0')}`,
+                      );
+                      if (currentHour < startHour) {
+                        return (
+                          <Alert className="bg-blue-300">
+                            <AlertTitle className="flex items-center gap-2">
+                              <TriangleAlert className="text-blue-600" />
+                              NOTICE: Too Early to Serve
+                            </AlertTitle>
+                            <AlertDescription>
+                              <p>
+                                Please come between {formatHour(startHour)} and{' '}
+                                {formatHour(endHour)} to redeem this meal.
+                              </p>
+                            </AlertDescription>
+                          </Alert>
+                        );
+                      }
+
+                      if (currentHour > endHour) {
+                        return (
+                          <Alert className="bg-red-300">
+                            <AlertTitle className="flex items-center gap-2">
+                              <TriangleAlert className="text-red-600" />
+                              ERROR: Meal Serving Time Passed
+                            </AlertTitle>
+                            <AlertDescription>
+                              <p>
+                                The meal serving time for this coupon has ended.
+                              </p>
+                            </AlertDescription>
+                          </Alert>
+                        );
+                      }
+
+                      if (couponDoc.docstatus === 1) {
+                        return (
+                          <Alert className="bg-red-300">
+                            <AlertTitle className="flex items-center gap-2">
+                              <TriangleAlert className="text-red-600" />
+                              ERROR: Coupon Already Consumed
+                            </AlertTitle>
+                            <AlertDescription>
+                              <>
+                                <p>Type: {couponDoc.title}</p>
+                                <p>Served By: {couponDoc.served_by}</p>
+                                <p>
+                                  Serving Time:{' '}
+                                  {couponDoc.modified.slice(10).slice(0, 9)}
+                                </p>
+                              </>
+                            </AlertDescription>
+                          </Alert>
+                        );
+                      }
+                      () => {
+                        setDisableServe(false);
+                        return (
+                          <>
+                            <Alert className="bg-green-300">
+                              <AlertTitle className="flex items-center gap-2">
+                                <CookingPot className="text-green-600" />
+                                SUCCESS: Meal Ready to Be Served
+                              </AlertTitle>
+                              <AlertDescription>
+                                <>
+                                  <p>Type: {title}</p>
+                                  <p>Time: {coupon_time}</p>
+                                  <p>Date: {coupon_date}</p>
+                                </>
+                              </AlertDescription>
+                            </Alert>
+                            {!loading ? (
+                              <Button
+                                onClick={() => handleServeCoupon(couponDoc)}
+                                className="w-full"
+                                disabled={disableServe}
+                              >
+                                Serve Meal
+                              </Button>
+                            ) : (
+                              <Button
+                                onClick={() => handleServeCoupon(couponDoc)}
+                                className="w-full"
+                              >
+                                <Loader2 className="animate-spin" />
+                                Serving Meal....
+                              </Button>
+                            )}
+                          </>
+                        );
+                      };
+                    })()}
+
+                    <Button
+                      onClick={handleReset}
+                      variant="outline"
+                      className="w-full"
+                    >
+                      Scan Another
+                    </Button>
+                  </div>
+                )
               )}
             </div>
           </CardContent>
         </Card>
         <div className="mx-auto w-full max-w-md">
-          <ErrorBanner error={apiCreateError} />
+          <ErrorBanner error={apiError} />
         </div>
       </div>
     </div>
