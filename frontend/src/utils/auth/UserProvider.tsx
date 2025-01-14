055aca06881a51e32ac52654b7beb5a078ff5f4b
Vinay Yadav <vinay@bytepanda.in>
2025-01-14 10:31:57 +0000

    chore : "changed provider to fetch id"

--- frontend/src/utils/auth/UserProvider.tsx
+++ frontend/src/utils/auth/UserProvider.tsx
@@ -1,12 +1,19 @@
-import { useFrappeAuth, useSWRConfig } from 'frappe-react-sdk';
-import { FC, PropsWithChildren } from 'react';
+import {
+  useFrappeAuth,
+  useFrappeGetDoc,
+  useFrappePostCall,
+  useSWRConfig,
+} from 'frappe-react-sdk';
+import { FC, PropsWithChildren, useEffect, useState } from 'react';
 import { createContext } from 'react';
+import { isHotpotAdmin, isHotpotServer, isHotpotUser } from '../roles';
 
 interface UserContextProps {
   isLoading: boolean;
   currentUser: string;
   logout: () => Promise<void>;
   updateCurrentUser: VoidFunction;
+  userId: string;
 }
 
 export const UserContext = createContext<UserContextProps>({
@@ -14,11 +21,37 @@ export const UserContext = createContext<UserContextProps>({
   isLoading: false,
   logout: () => Promise.resolve(),
   updateCurrentUser: () => {},
+  userId:''
 });
 
 export const UserProvider: FC<PropsWithChildren> = ({ children }) => {
   const { mutate } = useSWRConfig();
   const { logout, currentUser, updateCurrentUser, isLoading } = useFrappeAuth();
+  const [userId, setUserId] = useState('')
+  const { call: getUser } = useFrappePostCall(
+    'hotpot.api.users.get_hotpot_user_by_email',
+  );
+
+  const fetchUserDetails = async (currentUser: string) => {
+    if (currentUser) {
+      console.log(currentUser);
+      try {
+        const response = await getUser({ email: currentUser });
+        const {name} = response.message.data;
+        setUserId(name)
+      } catch (error) {
+        console.error('Error fetching user:', error);
+      }
+    } else {
+      console.log('No current user provided');
+    }
+  };
+  useEffect(() => {
+    console.log(isHotpotUser())
+    if (isHotpotUser() && !isHotpotAdmin() && !isHotpotServer() && currentUser?.trim()) {
+      fetchUserDetails(currentUser);
+    }
+  }, [currentUser]);
 
   const handleLogout = async () => {
     localStorage.removeItem('app-cache');
@@ -59,6 +92,7 @@ export const UserProvider: FC<PropsWithChildren> = ({ children }) => {
         updateCurrentUser,
         logout: handleLogout,
         currentUser: currentUser ?? '',
+        userId,
       }}
     >
       {children}
