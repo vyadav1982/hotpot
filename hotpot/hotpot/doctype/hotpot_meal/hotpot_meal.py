c17a3cd4f3309d1f47360d1e5bb21519f4f8dba1
Vinay Yadav <vinay@bytepanda.in>
2025-01-28 08:17:51 +0000

    chore: "automatically generated code"

---
+++ hotpot/hotpot/doctype/hotpot_meal/hotpot_meal.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2025, Bytepanda Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class HotpotMeal(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+		from hotpot.hotpot.doctype.hotpot_coupons.hotpot_coupons import HotpotCoupons
+
+		buffer_coupon_count: DF.Int
+		coupons: DF.Table[HotpotCoupons]
+		day: DF.Data | None
+		end_time: DF.Data | None
+		is_active: DF.Check
+		meal_date: DF.Date | None
+		meal_items: DF.Data | None
+		meal_title: DF.Data | None
+		start_time: DF.Data | None
+	# end: auto-generated types
+	pass
