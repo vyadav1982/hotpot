451ab075611c4e12e88668b982eb0eb05b12b160
Shashikant Yadav <sashikant12rao@gmail.com>
2025-02-12 09:34:31 +0000

    api:"fixed circular reference error"

--- hotpot/api/meal.py
+++ hotpot/api/meal.py
@@ -6,38 +6,7 @@ import pytz
 
 from ..api.coupons import update_coupon_status
 from ..api.users import *
-
-
-def get_user_timezone():
-	from pytz import timezone
-
-	user_info = frappe._dict()
-	frappe.utils.add_user_info(frappe.session.user, user_info)
-	return timezone(
-		frappe.get_cached_value("User", frappe.session.user, "time_zone")
-		or frappe.utils.get_system_timezone()
-	)
-
-def get_utc_datetime_str(date_str):
-	local_tz = get_user_timezone()
-	local_datetime = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
-	localized_datetime = local_tz.localize(local_datetime)
-	utc_datetime = localized_datetime.astimezone(pytz.utc)
-	utc_datetime_str = utc_datetime.strftime("%Y-%m-%d %H:%M:%S")
-	return utc_datetime_str
-
-def get_utc_date(date_str):
-	date_str=date_str.strftime("%Y-%m-%d %H:%M:%S")
-	utc_datetime_str = get_utc_datetime_str(date_str)
-	utc_date = datetime.strptime(utc_datetime_str, "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d")
-	return utc_date
-
-def get_utc_time(date_str):
-	date_str=date_str.strftime("%Y-%m-%d %H:%M:%S")
-	utc_datetime_str = get_utc_datetime_str(date_str)
-	utc_time = datetime.strptime(utc_datetime_str, "%Y-%m-%d %H:%M:%S").strftime("%H:%M:%S")
-	return utc_time
-
+from hotpot.utils.utc_time import *
 
 def set_response(http_status_code, status, message, data=None):
 	frappe.local.response["http_status_code"] = http_status_code
