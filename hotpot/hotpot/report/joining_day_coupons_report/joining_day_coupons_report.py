c44d3c2eb6048f2fa7b42d93fe9e29a285933ba4
Shashikant Yadav <sashikant12rao@gmail.com>
2025-03-10 10:25:53 +0000

    api:"created joining day coupon report"

---
+++ hotpot/hotpot/report/joining_day_coupons_report/joining_day_coupons_report.py
@@ -0,0 +1,73 @@
+# Copyright (c) 2025, Bytepanda Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+import frappe
+from frappe.utils import getdate
+
+from ....utils.utc_time import *
+
+
+def execute(filters=None):
+    if not filters:
+        filters = {}
+
+    start_date = filters.get("from_date")
+    end_date = filters.get("to_date")
+    vendor_id = filters.get("vendor_id")
+
+    if not start_date or not end_date:
+        frappe.throw("Please select both Start Date and End Date")
+
+    start_date = get_utc_datetime_obj(f"{start_date} 00:00:00")
+    end_date = get_utc_datetime_obj(f"{end_date} 23:59:59")
+
+    columns = [
+        {"label": "Meal Id", "fieldname": "meal_id", "fieldtype": "Data", "width": 120},
+        {"label": "Meal Title", "fieldname": "meal_title", "fieldtype": "Data", "width": 200},
+        {"label": "Vendor Id", "fieldname": "vendor_id", "fieldtype": "Data", "width": 120},
+        {"label": "Vendor Name", "fieldname": "vendor_name", "fieldtype": "Data", "width": 150},
+        {"label": "Coupon Id", "fieldname": "coupon_id", "fieldtype": "Data", "width": 120},
+        {"label": "Coupon Date", "fieldname": "coupon_date", "fieldtype": "Date", "width": 120},
+        {"label": "Employee Id", "fieldname": "employee_id", "fieldtype": "Data", "width": 120},
+        {"label": "Employee Name", "fieldname": "employee_name", "fieldtype": "Data", "width": 150},
+    ]
+
+    query = """
+        SELECT
+            hm.name AS meal_id,
+            hm.meal_title AS meal_title,
+            vendor.employee_id AS vendor_id,
+            vendor.employee_name AS vendor_name,
+            hc.name AS coupon_id,
+            hc.coupon_date AS coupon_date,
+            employee.employee_id AS employee_id,
+            employee.employee_name AS employee_name
+        FROM
+            `tabHotpot Coupons` AS hc
+        INNER JOIN
+            `tabHotpot Meal` AS hm ON hm.name = hc.parent
+        LEFT JOIN
+            `tabHotpot User` AS vendor ON vendor.name = hm.vendor_id
+        LEFT JOIN
+            `tabHotpot User` AS employee ON employee.name = hc.employee_id
+        WHERE
+            hc.joining_day = 1
+            AND hc.coupon_date BETWEEN %s AND %s
+    """
+
+    params = [start_date, end_date]
+
+    if vendor_id:
+        query += " AND hm.vendor_id = %s"
+        params.append(vendor_id)
+
+    query += " ORDER BY hc.coupon_date DESC"
+
+    data = frappe.db.sql(query, tuple(params), as_dict=True)
+
+    return columns, data
+
+
