16decb0a3044b02a27e30d38977d467c578f68fa
Shashikant Yadav <sashikant12rao@gmail.com>
2025-03-11 06:27:37 +0000

    api:"created guest meal consumption report"

---
+++ hotpot/hotpot/report/guest_meal_consumption_report/guest_meal_consumption_report.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2025, Bytepanda Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Guest Meal Consumption Report"] = {
+	filters: [
+    {
+      fieldname: "vendor_id",
+      label: __("Vendor"),
+      fieldtype: "Link",
+      width: "80",
+      options: "Hotpot User",
+      get_query: () => {
+        return {
+          query: get_all_vendor,
+        };
+      },
+    },
+    {
+      fieldname: "from_date",
+      label: __("From Date"),
+      fieldtype: "Date",
+      width: "80",
+      reqd: 1,
+      default: frappe.datetime.add_days(frappe.datetime.get_today(), -1),
+    },
+    {
+      fieldname: "to_date",
+      label: __("To Date"),
+      fieldtype: "Date",
+      width: "80",
+      reqd: 1,
+      default: frappe.datetime.get_today(),
+    },
+  ],
+};
