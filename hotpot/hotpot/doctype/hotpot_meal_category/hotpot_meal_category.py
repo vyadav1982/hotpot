20a63ec4ca055ec79b8db1564d74218a39b10b21
Shashikant Yadav <sashikant12rao@gmail.com>
2025-04-18 07:35:31 +0000

    created meal category doctype  and meal type doctype and added category field in meal doctype

---
+++ hotpot/hotpot/doctype/hotpot_meal_category/hotpot_meal_category.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2025, Bytepanda Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class HotpotMealCategory(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		cancellation_time: DF.Int
+		end_time: DF.Datetime | None
+		is_active: DF.Check
+		lead_time: DF.Int
+		start_time: DF.Datetime | None
+		type: DF.Link | None
+	# end: auto-generated types
+	pass
