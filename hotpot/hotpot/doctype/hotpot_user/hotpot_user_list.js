9edd18d64cfd3f323d7c35fa59726f59a3a7ea15
Shashikant Yadav <sashikant12rao@gmail.com>
2025-04-23 12:44:21 +0000

    added button again

--- hotpot/hotpot/doctype/hotpot_user/hotpot_user_list.js
+++ hotpot/hotpot/doctype/hotpot_user/hotpot_user_list.js
@@ -21,35 +21,35 @@ frappe.listview_settings["Hotpot User"] = {
             }
         });
 
-        // let disableBtn = listview.page.add_inner_button(__("Disable User"), function () {
-        //     let selectedUsers = listview.get_checked_items().map(doc => doc.name);
+        let disableBtn = listview.page.add_inner_button(__("Disable User"), function () {
+            let selectedUsers = listview.get_checked_items().map(doc => doc.name);
 
-        //     if (selectedUsers.length > 0) {
-        //         frappe.call({
-        //             method: "hotpot.hotpot.doctype.hotpot_user.disable_users",
-        //             freeze: true,
-        //             args: {
-        //                 user_list: selectedUsers
-        //             },
-        //             callback: function () {
-        //                 frappe.show_alert({
-        //                     message: __("Users Disabled Successfully"),
-        //                     indicator: "green"
-        //                 }, 5);
-        //                 listview.refresh();
-        //             }
-        //         });
-        //     } else {
-        //         frappe.msgprint(__("Please select at least one user to disable."));
-        //     }
-        // });
+            if (selectedUsers.length > 0) {
+                frappe.call({
+                    method: "hotpot.hotpot.doctype.hotpot_user.disable_users",
+                    freeze: true,
+                    args: {
+                        user_list: selectedUsers
+                    },
+                    callback: function () {
+                        frappe.show_alert({
+                            message: __("Users Disabled Successfully"),
+                            indicator: "green"
+                        }, 5);
+                        listview.refresh();
+                    }
+                });
+            } else {
+                frappe.msgprint(__("Please select at least one user to disable."));
+            }
+        });
 
-        // disableBtn.hide();
+        disableBtn.hide();
 
-        // listview.page.wrapper.on("change", ":checkbox", function () {
-        //     let selectedUsers = listview.get_checked_items();
-        //     disableBtn.toggle(selectedUsers.length > 0);
-        // });
+        listview.page.wrapper.on("change", ":checkbox", function () {
+            let selectedUsers = listview.get_checked_items();
+            disableBtn.toggle(selectedUsers.length > 0);
+        });
     },
     refresh: function (listview) {
         listview.page.clear_menu();
