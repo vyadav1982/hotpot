a0b75558f8b18f6e7349e092840224ec03430bcd
Shashikant Yadav <sashikant12rao@gmail.com>
2025-04-24 05:58:44 +0000

    fixed some ui date issues

--- hotpot/hotpot/doctype/hotpot_meal_category/hotpot_meal_category_list.js
+++ hotpot/hotpot/doctype/hotpot_meal_category/hotpot_meal_category_list.js
@@ -18,6 +18,10 @@ frappe.listview_settings["Hotpot Meal Category"] = {
             });
            
         }
+        listview.settings.formatters = {
+            start_time: formatUtcToLocal,
+            end_time: formatUtcToLocal,
+        };
           
     },
     refresh: function (listview) {
@@ -33,4 +37,20 @@ frappe.listview_settings["Hotpot Meal Category"] = {
             }
         }
     },
+}
+function formatUtcToLocal(utc_datetime) {
+    if (!utc_datetime) return "";
+
+    let user_timezone = frappe.sys_defaults.time_zone || Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    let date = new Date(utc_datetime + "Z");
+
+    let localTime = date.toLocaleTimeString("en-US", {
+        timeZone: user_timezone,
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true
+    });
+
+    return localTime;
 }
