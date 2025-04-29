a1e4f474d0c3afc00d0b6f45cd49b0921f93e94a
Shashikant Yadav <sashikant12rao@gmail.com>
2025-04-29 05:49:28 +0000

    chagned the ui for all roles of data import doctype

---
+++ hotpot/public/js/data_import_list.js
@@ -0,0 +1,45 @@
+frappe.listview_settings["Data Import"] = {
+    onload(listview) {
+        console.log("Data Import List View Loaded");
+        const roles = frappe.user_roles;
+        const isAdmin = roles.includes("Administrator");
+        const doctype = this.doctype;
+        if (!isAdmin) {
+            listview.page.clear_menu();
+            listview.page.clear_actions();
+            // listview.page.hide_menu();
+            // listview.page.hide_actions_menu();
+            listview.page.add_menu_item(__("Add Data Import", null, "Button in list view menu"), function () {
+                if (!frappe.boot.read_only && listview.can_create) {
+                    frappe.new_doc("Data Import");
+                } else {
+                    frappe.msgprint(__("You do not have permission to create a Hotpot User."));
+                }
+            });
+
+        }
+    },
+    refresh: function (listview) {
+        const roles = frappe.user_roles;
+        const doctype = this.doctype;
+        const isAdmin = roles.includes("Administrator");
+        if(!isAdmin){
+            listview.page.clear_menu();
+            listview.page.clear_actions();
+            // listview.page.hide_menu();
+            // listview.page.hide_actions_menu();
+            listview.page.add_menu_item(__("Add Data Import", null, "Button in list view menu"), function () {
+                if (!frappe.boot.read_only && listview.can_create) {
+                    frappe.new_doc("Data Import");
+                } else {
+                    frappe.msgprint(__("You do not have permission to create a Hotpot User."));
+                }
+            });
+            listview.toggle_actions_menu_button = function (toggle) {
+                return
+            }
+        }
+    },
+
+};
+
