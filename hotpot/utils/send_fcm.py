e2d872ae266987f70fa13276774f27dc59475139
Shashikant Yadav <sashikant12rao@gmail.com>
2025-05-06 16:13:11 +0000

    fixed syntax issue

--- hotpot/utils/send_fcm.py
+++ hotpot/utils/send_fcm.py
@@ -10,12 +10,12 @@ from firebase_config import messaging
 
 
 def send_notification_by_token(device_token, title, body):
-    message = messaging.Message(
-        notification = messaging.Notification(
-            title=title,
-            body=body,
-        ),
-        token = device_token,
-    )
-    response = messaging.send(message)
-    print("Successfully send message:", response)f
+	message = messaging.Message(
+		notification = messaging.Notification(
+			title=title,
+			body=body,
+		),
+		token = device_token,
+	)
+	response = messaging.send(message)
+	print("Successfully send message:", response)
