58375321c55b29dfea20084299992ab69c068f1c
Shashikant Yadav <sashikant12rao@gmail.com>
2025-05-22 13:59:23 +0000

    fixed meal rating last item issue

--- hotpot/api/coupons.py
+++ hotpot/api/coupons.py
@@ -637,6 +637,7 @@ def get_all_coupons(
 					hm.meal_items,
 					U.full_name AS vendor_name,
 					U2.full_name,
+					hm.vendor_id,
 					mt.type,
 					hc.*
 				FROM
@@ -786,14 +787,17 @@ def get_all_coupons(
 				)
 				item_listing = coupon.get("item_listing",[])
 				for item in item_listing:	
-					if(all_ratings and item["id"] == all_ratings[0]["meal_item"]):
-						items_rating.append({
-							"item_id": item["id"],
-							"item_name": item["title"],
-							"rating": all_ratings[0]["rating"]*5,
-							"review": all_ratings[0]["review"]
-						})
+					for rating in all_ratings:
+						if item["id"] == rating["meal_item"]:
+							items_rating.append({
+								"item_id": item["id"],
+								"item_name": item["title"],
+								"rating": rating["rating"] * 5,
+								"review": rating["review"]
+							})
+
 				coupon["items_rating"] = items_rating
+
 
 			set_response(200, True, "Coupons fetched successfully", coupons)
 			return
