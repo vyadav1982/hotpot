62970b7b0ecf801ced24f7d8cd5f0c6d2a5c532e
Shashikant Yadav <sashikant12rao@gmail.com>
2025-06-20 05:24:23 +0000

    added employee role

--- hotpot/hooks.py
+++ hotpot/hooks.py
@@ -42,6 +42,7 @@ fixtures = [
 					"Hotpot Vendor",
 					"Hotpot HR",
 					"Hotpot Finance",
+					"Employee",
 				],
 			]
 		},
@@ -58,6 +59,7 @@ fixtures = [
 					"Hotpot Vendor",
 					"Hotpot HR",
 					"Hotpot Finance",
+					"Employee",
 				],
 			],
 		},
@@ -75,6 +77,7 @@ fixtures = [
 					"Hotpot Vendor",
 					"Hotpot HR",
 					"Hotpot Finance",
+					"Employee",
 				],
 			],
 		},
