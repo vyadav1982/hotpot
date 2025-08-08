a80b260ee70aa4a31a35dba0b313b1d2dc2e7614
Shashikant Yadav <sashikant12rao@gmail.com>
2025-08-08 08:59:52 +0000

    skipped coupons with zero weight

--- hotpot/tasks/hotpot_tasks.py
+++ hotpot/tasks/hotpot_tasks.py
@@ -124,6 +124,8 @@ def update_meals_future():
 							if coupon_doc.coupon_status != "1":
 								continue
 							prev_weight = coupon_doc.coupon_weight
+							if not prev_weight:
+								continue
 							new_weight = categories.get(meal_doc.category)
 							user_doc = frappe.get_doc("Hotpot User", coupon.employee_id)
 
