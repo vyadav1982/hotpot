70d8e875cfa49a79d1c3a5e8aa89e7c92e407d2d
Shashikant Yadav <sashikant12rao@gmail.com>
2025-08-09 03:06:41 +0000

    added validatio in before save for meal rate

--- hotpot/hotpot/doctype/hotpot_meal/hotpot_meal.py
+++ hotpot/hotpot/doctype/hotpot_meal/hotpot_meal.py
@@ -97,18 +97,19 @@ class HotpotMeal(Document):
 			if duplicate_exists:
 				frappe.throw(f"A meal with category '{self.category}' already exists on {self.meal_date}.")
 
-			# vendor = frappe.get_doc("Hotpot User", self.vendor_id)
-			# if not vendor:
-			# 	frappe.throw(_("Vendor {0} does not exist.").format(self.vendor_id))
-			# category_doc = frappe.get_doc("Hotpot Meal Category", self.category)
-			# self.meal_weight = category_doc.meal_rate
-			# self.actual_meal_rate = category_doc.meal_rate
+			vendor = frappe.get_doc("Hotpot User", self.vendor_id)
+			if not vendor:
+				frappe.throw(_("Vendor {0} does not exist.").format(self.vendor_id))
+			category_doc = frappe.get_doc("Hotpot Meal Category", self.category)
+			self.meal_weight = category_doc.meal_rate
+			self.actual_meal_rate = category_doc.meal_rate
+			meal_date = get_local_datetime_obj(self.meal_date)
 
-			# for row in vendor.category_prices:
-			# 	if row.category == self.category:
-			# 		self.meal_weight = row.discounted_rate
-			# 		self.actual_meal_rate = row.actual_rate
-			# 		break
+			for row in vendor.category_prices:
+				if row.category == self.category and row.applicable_from and getdate(meal_date) >= getdate(row.applicable_from):
+					self.meal_weight = row.discounted_rate
+					self.actual_meal_rate = row.actual_rate
+					break
 
 		if is_frappe_ui_request() or frappe.flags.in_import:
 			self.menu_items = []
@@ -178,7 +179,7 @@ class HotpotMeal(Document):
 		current_datetime = get_local_datetime_obj(datetime.utcnow())
 
 		for row in vendor.category_prices:
-			if row.category == self.category and row.applicable_from and getdate(self.meal_date) >= getdate(row.applicable_from):
+			if row.category == self.category and row.applicable_from and getdate(meal_date) >= getdate(row.applicable_from):
 				self.meal_weight = row.discounted_rate
 				self.actual_meal_rate = row.actual_rate
 				break
