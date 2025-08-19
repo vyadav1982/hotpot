eec640de3c0957b18861605df5184f5d45bec42e
Shashikant Yadav <sashikant12rao@gmail.com>
2025-08-19 06:20:35 +0000

    fixed coupon count issue

--- hotpot/api/meal.py
+++ hotpot/api/meal.py
@@ -573,7 +573,7 @@ def get_meals(date, vendor_id=None, page=1, limit=10, for_kiosk=False):
 					if c.coupon_date.date() == date_param_utc and c.coupon_status != "2"
 				]
 			for coupon in meal_doc.coupons:
-				if coupon.coupon_status != "2":
+				if coupon.coupon_status != "2" or (coupon.approval_id and coupon.status=='Pending'):
 					meal["total_coupons"] += 1
 
 			# all_ratings = frappe.get_all(
