2dde1c7a4306f0cb804a9e333d862a3acadfc483
Shashikant Yadav <sashikant12rao@gmail.com>
2025-09-17 13:52:50 +0000

    partially completed review report

--- hotpot/hotpot/report/items_review_report/items_review_report.py
+++ hotpot/hotpot/report/items_review_report/items_review_report.py
@@ -1,9 +1,77 @@
 # Copyright (c) 2025, Bytepanda Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
 
+
+
+import frappe, json
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+	if not filters:
+		filters = {}
+
+	start_date = filters.get("from_date")
+	end_date = filters.get("to_date")
+	vendor_id = filters.get("vendor_id")
+
+	if not start_date or not end_date:
+		frappe.throw("Please select both Start Date and End Date")
+
+	columns = [
+		{"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 200},
+		{"label": "Vendor", "fieldname": "vendor_id", "fieldtype": "Data", "width": 180},
+		{"label": "Average Rating", "fieldname": "avg_rating", "fieldtype": "Data", "width": 150},
+		{"label": "All Feedbacks", "fieldname": "all_feedbacks", "fieldtype": "HTML", "width": 500},
+	]
+
+	where_clause = "1=1"
+	if vendor_id:
+		where_clause += " AND hi.vendor_id = %(vendor_id)s"
+
+	frappe.db.sql("SET SESSION group_concat_max_len = 1000000")
+
+	query = f"""
+		SELECT
+			hi.name AS item_id,
+			hi.item_name,
+			hi.vendor_id,
+			COUNT(hi.name) AS total_feedback,
+			ROUND(AVG(hr.rating) * 5, 1) AS avg_rating,
+			GROUP_CONCAT(
+				CONCAT(hu.full_name, ' (', hu.name, '): ', hr.review)
+				SEPARATOR '||'
+			) AS all_reviews
+		FROM `tabHotpot Meal Menu Items Rating` AS hr
+		INNER JOIN `tabHotpot Meal Items` AS hi ON hi.name = hr.meal_item
+		INNER JOIN `tabHotpot User` AS hu ON hu.name = hr.employee
+		WHERE {where_clause}
+		AND DATE(hr.creation) BETWEEN %(start_date)s AND %(end_date)s
+		AND hr.review IS NOT NULL
+		AND hr.review != ''
+		GROUP BY hi.name, hi.vendor_id
+	"""
+
+	data = frappe.db.sql(query, {"start_date": start_date, "end_date": end_date, "vendor_id": vendor_id}, as_dict=True)
+
+	result = []
+	for d in data:
+		stars = "⭐" * int(round(d.avg_rating or 0))
+
+		feedbacks = []
+		if d.all_reviews:
+			for idx, review in enumerate(d.all_reviews.split("||"), start=1):
+				review = review.strip()
+				if review:  # skip empty ones
+					feedbacks.append(f"<li>{review}</li>")
+
+		feedbacks_html = "<ul style='margin:0; padding-left:16px;'>" + "".join(feedbacks) + "</ul>" if feedbacks else "No Feedback"
+
+		result.append({
+			"item_name": d.item_name,
+			"vendor_id": d.vendor_id,
+			"avg_rating": f"{d.avg_rating} ({stars})",
+			"all_feedbacks": feedbacks_html
+		})
+
+
+	return columns, result
