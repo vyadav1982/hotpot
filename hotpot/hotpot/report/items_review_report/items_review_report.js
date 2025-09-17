5c9776000c0dc78f5077ad8f42f1b4003f74bc85
Shashikant Yadav <sashikant12rao@gmail.com>
2025-09-17 10:54:26 +0000

    created new report for meal items review

---
+++ hotpot/hotpot/report/items_review_report/items_review_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Bytepanda Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Items Review Report"] = {
+	"filters": [
+
+	]
+};
